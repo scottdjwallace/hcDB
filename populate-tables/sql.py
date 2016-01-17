e60e8d03631e40ef84c35aa99cfc39a0e2906a1e
Scott Wallace <ecosse_18@hotmail.com>
2016-01-16 22:58:55 -0500

    updated for sql python script

---
+++ populate-tables/sql.py
@@ -0,0 +1,5 @@
+# Script will take input and use it to create SQL insert statements,
+# then save in a text file in order to use in phpMyAdmin
+
+# SQL statement example
+# INSERT INTO cards VALUES (...);
