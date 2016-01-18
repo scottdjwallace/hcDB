d6107c3288e1dbff1b886f0c5300420cc2f59d27
Scott Wallace <ecosse_18@hotmail.com>
2016-01-18 18:07:21 -0500

    connection and results from database set up

--- Engine/src/hcdb/Main.java
+++ Engine/src/hcdb/Main.java
@@ -6,18 +6,11 @@ public class Main {
 	
 	public static void main(String[] Args){
 		
-		// Connecting to Database
-		String url = "jdbc:mysql://localhost:3306/Javabase";
-		String username = "java";
-		String password = "password";
-	
-		System.out.println("Connecting database...");
-	
-		try (Connection connection = DriverManager.getConnection(url, username, password)) {
-		    System.out.println("Database connected!");
-		} catch (SQLException e) {
-		    throw new IllegalStateException("Cannot connect the database!", e);
-		}
+		Database db = new Database();
+		ResultSet rs = db.execute("SELECT * FROM cards");
+		db.disconnect();
+		
+
 	}
 
 }
