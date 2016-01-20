0857999c28a1c2592b5f2cb2a31f59c0c6919a94
Scott Wallace <ecosse_18@hotmail.com>
2016-01-19 22:18:58 -0500

    removed unused project

--- populate-tables/sql.py
+++
@@ -1,5 +0,0 @@
-# Script will take input and use it to create SQL insert statements,
-# then save in a text file in order to use in phpMyAdmin
-
-# SQL statement example
-# INSERT INTO cards VALUES (...);
